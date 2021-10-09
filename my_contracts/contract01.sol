88947c0c3600c4a6e1363c1df26a3ca88b6e9935
kaledae <himmatrai20@gmail.com>
2021-10-09 23:14:39 +0545

    initial commit

---
+++ my_contracts/contract01.sol
@@ -0,0 +1,9 @@
+// SPDX-Liscense-Identifier: UNLICENSED
+
+pragma solidity ^0.8.9;
+
+contract Contract01{
+    function fun(string memory data) pure public returns(string memory){
+        return data;
+    }
+}
---
+++ my_contracts/contract02.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.9;
+
+import 'contract01.sol';
+
+contract Contract02{
+    Contract01.fun
+    
+    
+}
